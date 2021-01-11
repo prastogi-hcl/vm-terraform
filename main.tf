5ec016c88f277f6c3e6be58afbe103b5f8148c69
prastogi-hcl <48212054+prastogi-hcl@users.noreply.github.com>
2021-01-12 00:18:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,37 +13,37 @@ data "vsphere_datacenter" "dc" {
 
 data "vsphere_datastore" "datastore" {
   name          = "vsanDatastore"
-  datacenter_id = "data.vsphere_datacenter.dc.id"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_resource_pool" "pool" {
   name          = "Anthos-QA"
-  datacenter_id = "data.vsphere_datacenter.dc.id"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
   name          = "ANTHOS-VM-MANAGEMENT-PG"
-  datacenter_id = "data.vsphere_datacenter.dc.id"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 #Data source for VM template
 data "vsphere_virtual_machine" "template" {
    name = "anthos-qa-jumpbox"
-   datacenter_id = "data.vsphere_datacenter.dc.id"
+   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
 name             = "jumpbox-12jan"
-resource_pool_id = "data.vsphere_resource_pool.pool.id"
-datastore_id     = "data.vsphere_datastore.datastore.id"
+resource_pool_id = data.vsphere_resource_pool.pool.id
+datastore_id     = data.vsphere_datastore.datastore.id
 wait_for_guest_net_timeout = 0
 wait_for_guest_ip_timeout  = 0
 num_cpus = 2
 memory   = 4096
-guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+guest_id = data.vsphere_virtual_machine.template.guest_id
 
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id = data.vsphere_network.network.id
   }
 
   disk {
@@ -53,7 +53,7 @@ guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
 
   #Included a clone attribute in the resource
   clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    template_uuid = data.vsphere_virtual_machine.template.id
 
     customize {
       linux_options{
@@ -61,7 +61,7 @@ guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
         domain = "hclcnlabs.com"
       }
       network_interface {
-        ipv4_address = var.ipv4_address
+        ipv4_address = "172.16.135.24"
         ipv4_netmask = "24"
       }
 
@@ -71,5 +71,3 @@ guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
     }
   }
 }
-
-
