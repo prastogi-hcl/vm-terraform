d545ff82388586d86fa3df49c2e055945deaa52e
root <parikshit.r@hcl.com>
2021-01-26 05:48:01 -0800

    first commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
-  user           = "var.vsphere_user"
-  password       = "var.vsphere_password"
-  vsphere_server = "var.vsphere_server"
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
@@ -33,7 +33,7 @@ data "vsphere_virtual_machine" "template" {
 }
 
 resource "vsphere_virtual_machine" "vm" {
-name             = "var.servername"
+name             = var.servername
 resource_pool_id = data.vsphere_resource_pool.pool.id
 datastore_id     = data.vsphere_datastore.datastore.id
 wait_for_guest_net_timeout = 0
@@ -57,11 +57,11 @@ guest_id = data.vsphere_virtual_machine.template.guest_id
 
     customize {
       linux_options{
-        host_name = "var.servername"
+        host_name = var.servername
         domain = "hclcnlabs.com"
       }
       network_interface {
-        ipv4_address = "var.ipv4_address"
+        ipv4_address = var.ipv4_address
         ipv4_netmask = "24"
       }
 
