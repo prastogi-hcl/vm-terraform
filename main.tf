e4a4ee62673310a1499e07e6b09ddbd1015c1950
prastogi-hcl <48212054+prastogi-hcl@users.noreply.github.com>
2021-01-27 13:48:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "vsphere" {
   user           = "administrator@vsphere.local"
   password       = "Fri@co123!"
-  vsphere_server = var.vsphere_server
+  vsphere_server = "var.vsphere_server"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
