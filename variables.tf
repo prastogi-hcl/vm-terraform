ec7eb38a68cf82f85b99d60b63151475d55f5b68
prastogi-hcl <48212054+prastogi-hcl@users.noreply.github.com>
2021-01-27 14:07:41 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "vsphere_server" {
+    description = "vsphere server for the environment - EXAMPLE: vcenter01.hosted.local"
+    default = "uslabpowvcs01.hclcnlabs.com"
+}
+variable "vsphere_user" {
+    description = "vsphere server for the environment - EXAMPLE: vsphereuser"
+    default = "administrator@vsphere.local"
+}
+variable "JumpboxIP" {
+    description = "vsphere server ipv4 address - EXAMPLE: 172.16.135.60"
+    default = "172.16.135.16"
+ }
+variable "vsphere_password" {
+    description = "vsphere server password for the environment"
+    default = "Fri@co123!"
+}
+variable "JumpboxName" {
+    description = "Server Name"
+    default = "jumpbox-27"
+}
